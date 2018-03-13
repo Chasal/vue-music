ceff69e997387a2e3c07a26d1205caf3989a534c
Chasal <lyz00054@163.com>
2018-03-13 23:15:18 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    Hello World!
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import 'common/stylus/variable.styl'
+  #app
+    color: $color-theme
+</style>
